7b73d6827759e9b1145de044199f8c23cbaf89ed
kevinelong <kevinelong@gmail.com>
2022-04-26 12:19:43 -0700

    app-routing.module add protected component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProtectedComponent } from './protected/protected.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: []
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
